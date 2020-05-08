d25de768c98985960ed9ad49e144ca3597fa5804
ShayakhmetovaElza <63636579+ShayakhmetovaElza@users.noreply.github.com>
2020-05-08 11:29:13 +0300

    project new

---
+++ Catalog new/Catalog/Catalog/Models/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Models
+{
+    public class Level
+    {        
+        public string NameLevel { get; set; }
+
+        public Level(string level)
+        {
+            this.NameLevel = level;
+        }
+    }
+}
---
+++ Catalog new/Catalog/Catalog/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Pattern> patterns { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        { }
+
+    }
+}
